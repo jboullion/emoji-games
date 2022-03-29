ac02f000089a61b93df90e324f1790dabad06119
jboullion <jboullion83@gmail.com>
2022-03-28 21:43:00 -0500

    Sockets working!

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,98 @@ import ModalSettings from './components/modals/Settings.vue';
 import Footer from './components/common/Footer.vue';
 import Header from './components/common/Header.vue';
 import ModalSearch from './components/modals/Search.vue';
+
+import { io, Socket } from 'socket.io-client';
+import { ref } from 'vue';
+
+type MemoryMessage = {
+  name: string;
+  text: string;
+};
+
+type MemoryClient = {
+  title: string;
+  name: string;
+  text: string;
+  messages: MemoryMessage[];
+  socket: Socket | null;
+};
+
+const clientData = ref<MemoryClient>({
+  title: 'Nestjs Websockets Chat',
+  name: '',
+  text: '',
+  messages: [],
+  socket: null,
+});
+
+const socket = io('http://localhost:3001');
+socket.on('msgToClient', (message) => {
+  receivedMessage(message);
+});
+
+function sendMessage() {
+  if (validateInput()) {
+    const message = {
+      name: clientData.value.name,
+      text: clientData.value.text,
+    };
+    socket.emit('msgToServer', message);
+    clientData.value.text = '';
+  }
+}
+function receivedMessage(message: MemoryMessage) {
+  clientData.value.messages.push(message);
+}
+
+function validateInput() {
+  return clientData.value.name.length > 0 && clientData.value.text.length > 0;
+}
+console.log('socket', socket);
 </script>
 
 <template>
   <div id="app-wrapper">
     <div class="container d-flex flex-fill flex-column justify-content-between">
       <Header />
+
+      <div class="row">
+        <div class="col-md-6 offset-md-3 col-sm-12">
+          <h1 class="text-center">{{ clientData.title }}</h1>
+          <br />
+          <div id="status"></div>
+          <div id="chat">
+            <input
+              type="text"
+              v-model="clientData.name"
+              id="username"
+              class="form-control"
+              placeholder="Enter name..."
+            />
+            <br />
+            <div class="card">
+              <div id="messages" class="card-block">
+                <ul>
+                  <li v-for="message of clientData.messages">
+                    {{ message.name }}: {{ message.text }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <br />
+            <textarea
+              id="textarea"
+              class="form-control"
+              v-model="clientData.text"
+              placeholder="Enter message..."
+            ></textarea>
+            <br />
+            <button id="send" class="btn" @click.prevent="sendMessage">
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
 
       <router-view></router-view>
 
